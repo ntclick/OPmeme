46c8b20a0c40a3f1c8b5dbf12266b5fc9f04e911
ntclick <tiphukonha@gmail.com>
2026-03-04 08:07:53 +0700

    Fix: Patch httpx.Client (sync) to bypass SSL error for client.llm.chat()

--- analysis/opengradient.py
+++ analysis/opengradient.py
@@ -49,6 +49,21 @@ else:
 
 import opengradient as og
 
+import httpx
+
+# Patch AsyncClient
+_orig_async_client_init = httpx.AsyncClient.__init__
+def _patched_async_client_init(self, *args, **kwargs):
+    kwargs["verify"] = False
+    _orig_async_client_init(self, *args, **kwargs)
+httpx.AsyncClient.__init__ = _patched_async_client_init
+
+# Patch Sync Client
+_orig_sync_client_init = httpx.Client.__init__
+def _patched_sync_client_init(self, *args, **kwargs):
+    kwargs["verify"] = False
+    _orig_sync_client_init(self, *args, **kwargs)
+httpx.Client.__init__ = _patched_sync_client_init
 
 
 
